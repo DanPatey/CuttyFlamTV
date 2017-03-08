3c5b782a466eb67d2091a225b9e296532276b06a
Dan Patey <Dan@StackSocial.com>
2017-03-08 15:03:15 -0800

    adding simple client to test cells, categories, and video playback

---
+++ client/templates/CuttyFlamTV.xml.js
@@ -0,0 +1,62 @@
+var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
+  <document>
+    <catalogTemplate>
+      <banner>
+        <title>CuttyFlamTV</title>
+      </banner>
+      <list>
+        <section>
+          <listItemLockup>
+            <title>Music Videos</title>
+            <decorationLabel>13</decorationLabel>
+            <relatedContent>
+              <grid>
+                <section>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/ray.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/ryan.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/matthijs.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/vicki.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/alexis.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/marin.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/chris.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/cesare.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/ellen.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/jake.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/kim.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/tammy.png" width="500" height="308" />
+                  </lockup>
+                  <lockup videoURL="">
+                    <img src="${this.BASEURL}images/saul.png" width="500" height="308" />
+                  </lockup>
+                </section>
+              </grid>
+            </relatedContent>
+          </listItemLockup>
+        </section>
+      </list>
+    </catalogTemplate>
+  </document>`
+}
